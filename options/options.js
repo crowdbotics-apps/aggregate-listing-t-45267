b28490dcdc821369ffa271bba3c486a204e2355b
Crowdbotics <team@crowdbotics.com>
2024-01-03 21:54:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aggregate-listing-t-45267",
+  url: "https://aggregate-listing-t-45267.botics.co",
+  api: "https://aggregate-listing-t-45267.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
